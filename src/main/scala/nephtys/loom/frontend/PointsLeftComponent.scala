577ecc8ec80aed0f0bb25b81b1e119eb3915fd69
nephtys <christopher.kaag@gmail.com>
2016-12-29 19:41:56 +0100

    began work on points left component

--- src/main/scala/nephtys/loom/frontend/PointsLeftComponent.scala
+++ src/main/scala/nephtys/loom/frontend/PointsLeftComponent.scala
@@ -1,7 +1,11 @@
 package nephtys.loom.frontend
 
+import angulate2.core.OnChanges.SimpleChanges
+import angulate2.core.OnChangesJS
 import angulate2.std._
 import nephtys.loom.protocol.vanilla.solar.Solar
+
+import scala.collection.immutable.SortedSet
 
 /**
   * Created by nephtys on 12/12/16.
@@ -10,7 +14,11 @@ import nephtys.loom.protocol.vanilla.solar.Solar
   selector = "points-left-vanilla",
   template =
     """ <div>
-      |this is the points left component
+      |<remark-component
+      | [title]="title"
+      | [remarksLowPrio]="lowPrio"
+      | [remarksHighPrio] = "hiPrio"
+      |></remark-component>
       |</div>
     """.stripMargin,
   styles = @@@(
@@ -21,10 +29,72 @@ import nephtys.loom.protocol.vanilla.solar.Solar
     """.stripMargin)
 
 )
-class PointsLeftComponent {
+class PointsLeftComponent extends OnChangesJS{
+
+
+  @Input
+  var title : String = "You may spend:"
 
   @Input
   var character : Solar = _
+
+  var lowPrio : Seq[String] = Seq.empty
+  var hiPrio : Seq[String] = Seq.empty
+
+
+  override def ngOnChanges(changes: SimpleChanges): Unit = {
+    if(character != null) {
+      lowPrio = generateLowPrio(character)
+      hiPrio = generateHighPrio(character)
+    }
+  }
+
+  private def generateLowPrio(solar : Solar) : Seq[String] = {
+    if (solar.stillInCharGen) {
+      val intimacies : Seq[String] = Seq.empty
+      val bonuspoints : Seq[String] = Seq.empty
+      val specialties : Seq[String] = Seq.empty
+
+      intimacies ++ specialties ++ bonuspoints
+    } else {
+      Seq.empty //is this even ever needed for anything? keep for later functional plugins
+    }
+  }
+
+  private def generateHighPrio(solar : Solar) : Seq[String] = {
+    if (solar.stillInCharGen) {
+      val attributes: Seq[String] = {
+        val physicals: Int = solar.attributes.physicals.map(_.dots.number.toInt).sum - 3
+        val socials: Int = solar.attributes.socials.map(_.dots.number.toInt).sum - 3
+        val mentals: Int = solar.attributes.mentals.map(_.dots.number.toInt).sum - 3
+        val vals = Seq(physicals, socials, mentals)
+        val max = vals.max
+        val min = vals.min
+        val middle = vals.sorted.apply(1)
+        if (vals.sorted.equals(Seq(4, 6, 8))) Seq.empty else {
+          Seq(s"Primary Attributes: $max/8",
+            s"Seconary Attributes: $middle/6",
+            s"Terniary Attributes: $min/4"
+          )
+        }
+
+      }
+
+      val abilityTypes: Seq[String] = Seq.empty
+      val abilities: Seq[String] = Seq.empty
+
+      val merits : Seq[String] = Seq.empty
+
+      val charms : Seq[String] = Seq.empty
+
+      attributes ++ abilityTypes ++ abilities ++ merits ++ charms
+
+    } else {
+      val experiences : Seq[String] = Seq.empty
+
+      experiences
+    }
+  }
 
   //TODO: implement whole
   //use remark control for this
@@ -45,12 +115,12 @@ class PointsLeftComponent {
   4) 5 favored abilities
   5) 1 supernal ability (again, only from valid abilities)
   6) 28 free ability points
-  7) 4 specialties
+  7) 4 specialties [low]
 
   8) 10 merits
   9) 15 charms
-  10) >= 4 Intimacies (how to model this?)
-  11) 15 Bonus Points
+  10) >= 4 Intimacies (how to model this?) [low]
+  11) 15 Bonus Points [low]
 
 
    */
